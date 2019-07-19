ec66125f0e150d625aab832039dacab48320c74a
roy-lau <roy-lau@foxmail.com>
2019-07-19 14:39:12 +0800

    dev-vp: 更新TODO

--- docs/.vuepress/theme/layouts/Files.vue
+++ docs/.vuepress/theme/layouts/Files.vue
@@ -3,17 +3,17 @@
         <v-flex d-flex md8 lg8>
             <v-timeline dense clipped align-top>
                 <v-slide-x-reverse-transition group hide-on-leave>
-                    <v-timeline-item v-for="(post, i) in postList" :color="years[i].color" v-if="post" :key="i"> <!-- fill-dot  -->
-                        <v-card :color="years[i].color" dark flat>
+                    <v-timeline-item v-for="(post, i) in postList" :color="colors[i]" v-if="post" :key="i"> <!-- fill-dot  -->
+                        <v-card :color="colors[i]" dark flat>
                             <v-card-title v-text="post.date" />
                             <v-list class="white">
                                 <template v-for="list in post.list" >
                                     <v-layout row wrap mt-2 ml-2>
                                         <v-flex xs10 sm10 md10 lg10 xl10 >
-                                            <router-link :to="list.path" :class="`${years[i].color}--text text--lighten-1 text-xs-left text--link title`" v-text="list.title" />
+                                            <router-link :to="list.path" :class="`${colors[i]}--text text--lighten-1 text-xs-left text--link title`" v-text="list.title" />
                                         </v-flex>
                                         <v-flex xs2 sm2 md2 lg2 xl2>
-                                            <v-chip small outline :color="years[i].color" v-text="list.time" class="px-2" />
+                                            <v-chip small outline :color="colors[i]" v-text="list.time" class="px-2" />
                                         </v-flex>
                                         <v-flex xs12 sm12 md12 lg12 xl12>
                                             <div class="grey--text text--darken-3 body-2 px-3" v-if="list.excerpt" v-html="list.excerpt"></div>
@@ -33,29 +33,7 @@
 export default {
     data() {
         return {
-            years: [
-                { icon: 'loyalty', color: 'orange' },
-                { icon: 'loyalty', color: 'pink' },
-                { icon: 'loyalty', color: 'deep-purple' },
-                { icon: 'loyalty', color: 'indigo' },
-                { icon: 'loyalty', color: 'blue' },
-                { icon: 'loyalty', color: 'teal' },
-                { icon: 'loyalty', color: 'light-blue' },
-                { icon: 'loyalty', color: 'cyan' },
-                { icon: 'loyalty', color: 'green' },
-                { icon: 'loyalty', color: 'light-green' },
-                { icon: 'loyalty', color: 'lime' },
-                { icon: 'loyalty', color: 'amber' },
-                { icon: 'loyalty', color: 'red' },
-                { icon: 'loyalty', color: 'yellow' },
-                { icon: 'loyalty', color: 'deep-orange' },
-                { icon: 'loyalty', color: 'brown' },
-                { icon: 'loyalty', color: 'blue-grey' },
-                { icon: 'loyalty', color: 'grey' },
-                { icon: 'loyalty', color: 'black' },
-                { icon: 'loyalty', color: 'white' },
-                { icon: 'loyalty', color: 'transparent' }
-            ],
+            colors: ['orange','pink','deep-purple','indigo','blue','teal','light-blue','cyan','green','light-green','lime','amber','red','yellow','deep-orange','brown','blue-grey','grey','black','white','transparent'],
             pageSize: 5, // 每页显示多少篇文章
             pageCurrent: 1, // 默认当前是在第几页
         }
