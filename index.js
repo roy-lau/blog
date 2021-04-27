f423654b62e454b9d55e12046253368ee3eee0d9
roylau <roy-lau@foxmail.com>
2021-04-27 22:31:56 +0800

    dev-yuque: 尝试读取语雀文档

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const Render = require("./src/render.js")
+const fs = require('fs')
 const SDK = require('@yuque/sdk');
 const client = new SDK({
   token: 'A4of6TlXsYRw6oOZm0iwTc1CN4XDOqqd81qxzwBR',
+  handler(res) {
+    // should handler error yourself
+    if (res.status !== 200) {
+      const err = new Error(res.data.message);
+      /* istanbul ignore next */
+      err.status = res.data.status || res.status;
+      err.code = res.data.code;
+      err.data = res;
+      throw err;
+    }
+    // return whatever you want
+    const { data, abilities } = res.data;
+    return data;
+  }
   // other options
 });
 
@@ -14,11 +29,20 @@ async function setup() {
 
 
   const r_repos = await repos.get({ namespace: "roylau/blog" });
-  console.log(r_repos)
+  fs.writeFileSync('./data/r_repos.json',JSON.stringify(r_repos,null,2))
+  // console.log(r_repos)
 
-  // for (let i = 0; i < r_repos.length; i++) {
-  //   const r_docs = await docs.get({ login: "roylau/blog" });
-  //   console.log(r_docs)
-  // }
+  const docsList = await docs.list({ namespace: "roylau/blog" })
+  fs.writeFileSync('./data/docsList.json',JSON.stringify(docsList,null,2))
+  // console.log(docsList)
+
+  const docsConentArr = []
+  for (let i = 0; i < docsList.length; i++) {
+    const r_docs = await docs.get({ namespace: "roylau/blog", slug: docsList[i].slug });
+    // console.log(r_docs)
+    docsConentArr.push(r_docs)
+  }
+  fs.writeFileSync('./data/docsConentArr.json',JSON.stringiÏfy(docsConentArr,null,2))
+
 }
 setup()
